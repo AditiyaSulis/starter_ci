c3dffb3e81d3224e2cd4ccf6be550e94df611905
AditiyaSulis <aditiyasulistiyani12@gmail.com>
2025-04-09 11:51:50 +0700

    modify Landing

--- application/controllers/api/Landing.php
+++ application/controllers/api/Landing.php
@@ -11,7 +11,6 @@ class Landing extends RestController{
 	{
 		parent::__construct();
 		$this->load->model('M_setting');
-		$this->load->model('M_products');
 		$this->load->model('M_product_homepage');
 		validate_header();
 
@@ -19,7 +18,7 @@ class Landing extends RestController{
 
 	//===========COMPANY PROFILE=============
 	/* Mengambil 1 Data Company Profile */
-	public function getOne_get()
+	public function company_get()
 	{
 
 		$company_profile = $this->M_setting->getCp_get();
@@ -37,61 +36,11 @@ class Landing extends RestController{
 		}
 	}
 
-	//===========PRODUCTS============
-	public function getAllVisibility_get()
-	{
-		$products = $this->M_products->findAllShow_get();
 
-		if ($products) {
-			$this->response([
-				'status' => true,
-				'data' => $products
-			], 200);
-		} else {
-			$this->response([
-				'status' => false,
-				'message' => 'Data tidak ditemukan'
-			], 404);
-		}
-	}
-
-	public function getAll_get()
-	{
-		$products = $this->M_products->findAll_get();
-
-		if ($products) {
-			$this->response([
-				'status' => true,
-				'data' => $products
-			], 200);
-		} else {
-			$this->response([
-				'status' => false,
-				'message' => 'Data tidak ditemukan'
-			], 404);
-		}
-	}
-
-	public function getById_get($id)
-	{
-		$product = $this->M_products->findById_get($id);
-
-		if ($product) {
-			$this->response([
-				'status' => true,
-				'data' => $product
-			], 200);
-		} else {
-			$this->response([
-				'status' => false,
-				'message' => 'Product not found'
-			], 404);
-		}
-	}
 
 
 	//===========Product Homepage=============
-	public function getAllHomepage_get()
+	public function product_get()
 	{
 		$products = $this->M_product_homepage->findAll_get();
 
@@ -108,22 +57,7 @@ class Landing extends RestController{
 		}
 	}
 
-	public function getByIdHomepage_get($id)
-	{
-		$product = $this->M_product_homepage->findById_get($id);
 
-		if ($product) {
-			$this->response([
-				'status' => true,
-				'data' => $product
-			], 200);
-		} else {
-			$this->response([
-				'status' => false,
-				'message' => 'Product not found'
-			], 404);
-		}
-	}
 
 
 }
